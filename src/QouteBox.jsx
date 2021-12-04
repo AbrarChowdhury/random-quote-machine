4c8a95360b57c2ae773abb069ecc39bb21543a3c
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2021-12-04 15:39:31 +0600

    font weight changed

--- src/QouteBox.jsx
+++ src/QouteBox.jsx
@@ -5,6 +5,7 @@ function QuoteBox({quote,update,color} ) {
 
     const styles={
         paper:{ maxWidth:"400px", minWidth:"300px", padding:"20px",color:color},
+        text:{fontSize:"1.5em", fontWeight:"700"},
         author:{float:"right", margin:"0"},
         button:{float:"left",background:color, color:"white", display:"block",'&:hover': {
        background: "grey",
@@ -14,7 +15,7 @@ function QuoteBox({quote,update,color} ) {
     return (
         <Fade in={true}>
         <Paper id="quote-box" style={styles.paper} elevation={5}>
-            <p id="text">{quote?.quote}</p>
+            <p id="text" style={styles.text} >"{quote?.quote}"</p>
             <p id="author" style={styles.author}>- {quote?.author}</p>
             <br/>
                 <Button sx={styles.button} id="new-quote" variant="contained" onClick={update}>new quote</Button>
